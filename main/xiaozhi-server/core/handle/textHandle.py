300b283858edf4e3c08ea28670f19cef980d98dc
sczx-win10 <wanyufeng970224@163.com>
2025-05-08 17:00:54 +0800

    update:修改大娃娃逻辑

--- main/xiaozhi-server/core/handle/textHandle.py
+++ main/xiaozhi-server/core/handle/textHandle.py
@@ -20,7 +20,7 @@ async def handleTextMessage(conn, message):
             await conn.websocket.send(message)
             return
         if msg_json["type"] == "hello":
-            await handleHelloMessage(conn)
+            await handleHelloMessage(conn, msg_json)
         elif msg_json["type"] == "abort":
             await handleAbortMessage(conn)
         elif msg_json["type"] == "listen":
@@ -29,6 +29,11 @@ async def handleTextMessage(conn, message):
                 conn.logger.bind(tag=TAG).debug(
                     f"客户端拾音模式：{conn.client_listen_mode}"
                 )
+            if "sensor" in msg_json:
+                conn.client_have_voice = True
+                conn.client_voice_stop = False
+                await handleSensorMessage(conn, msg_json)
+                return
             if msg_json["state"] == "start":
                 conn.client_have_voice = True
                 conn.client_voice_stop = False
@@ -138,3 +143,21 @@ async def handleTextMessage(conn, message):
                     )
     except json.JSONDecodeError:
         await conn.websocket.send(message)
+
+async def handleSensorMessage(conn, message):
+    first = bool(message["wakeup"])
+    sensor = message["sensor"]
+    value = int(message["sensor_value"])
+    msg = ''
+    if 'hug' in sensor:
+        msg = "[hug]"  # 用户拥抱
+    elif 'head' in sensor:
+        msg = "[head]"  # 用户摸头
+    elif 'left_hand' in sensor:
+        msg = "[sl]"  # 用户握手（左手）
+    elif 'right_hand' in sensor:
+        msg = "[sr]"  # 用户握手（右手）
+    conn.logger.bind(tag=TAG).info(f"传感器事件: {sensor}, 消息: {msg}, 值: {value}, 是否首次: {first}")
+    # if first:
+    #     msg += ""
+    await startToChat(conn, msg)
